23cb448b5eccd79786b7269c8a06a15091861185
ak-parser <81529142+ak-parser@users.noreply.github.com>
2021-09-28 11:44:43 +0300

    Create Buy and Check (for output Buy) class

---
+++ Buy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    class Buy
+    {
+        public Product Prod { get; private set; }
+        public int Count { get; private set; }
+        public double TotalPrice { get; private set; }
+        public double TotalWeight { get; private set; }
+
+
+        public Buy(Product product, int count)
+        {
+            Prod = (product == null) ? throw new ArgumentNullException("Product not set") : product;
+            Count = (count <= 0) ? throw new ArgumentNullException("The product name cannot be empty") : count;
+            TotalPrice = Prod.Price * Count;
+            TotalWeight = Prod.Weight * Count;
+        }
+    }
+}
---
+++ Check.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    sealed class Check
+    {
+        public Check()
+        {
+
+        }
+        public void print(Buy buy)
+        {
+            if (buy == null)
+                throw new ArgumentNullException("Buy not set");
+
+            Console.WriteLine($"Name: {buy.Prod.Name} Price: {buy.Prod.Price} Weight: {buy.Prod.Weight}\nCount: {buy.Count}\nTotal price: " +
+                    $"{buy.TotalPrice}\nTotal weight: {buy.TotalWeight}\n");
+        }
+    }
+
+    //Помилка при спробі наслідування
+    /*class CheckDerived : Check
+    {
+        public CheckDerived()
+        {
+
+        }
+    }*/
+}
