10686a05c0985a2dbe148d783c6c40d3271e339c
ak-parser <81529142+ak-parser@users.noreply.github.com>
2021-09-28 22:18:44 +0300

    Add ShowException class to show exception on console

---
+++ ShowException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    static class ShowException
+    {
+        static public void ConsoleWrite(string output)
+        {
+            Console.WriteLine($"Error: {output}");
+        }
+    }
+}
