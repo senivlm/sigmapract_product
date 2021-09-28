110f81a80196f7e384d01e80ab290434c31a67a7
ak-parser <81529142+ak-parser@users.noreply.github.com>
2021-09-28 11:22:26 +0300

    Create solution project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("Apple", 5.50, 0.120);
+            Product product2 = new Product("Sugar", 30, 1);
+            Product product3 = new Product("Bread", 15, 0.5);
+
+            Buy buy1 = new Buy(product1, 7);
+            Buy buy2 = new Buy(product2, 2);
+            Buy buy3 = new Buy(product3, 4);
+
+            Check check = new Check();
+            check.print(buy1);
+            check.print(buy2);
+            check.print(buy3);
+
+            Storage storage = new Storage();
+            storage.readInput();
+            storage.print();
+        }
+    }
+}
