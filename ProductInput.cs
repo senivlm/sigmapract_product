83da2f96e03c4ff81b9d53415f3bd4cab83a0115
ak-parser <81529142+ak-parser@users.noreply.github.com>
2021-10-06 00:05:11 +0300

    Add input from file, correcting input from console

--- ProductInput.cs
+++ ProductInput.cs
@@ -35,17 +35,18 @@ namespace Task1
                     switch(productType)
                     {
                         case 1:
-                            Console.WriteLine($"Введіть назву, ціну, вагу, категорію(Highest, First, Second) та вид(Lamb, Veal, Pork or Chicken) через пробіл");
+                            Console.WriteLine($"Введіть назву, ціну, вагу, термін придатності, дату виготовлення, " +
+                                $"категорію(Highest, First, Second) та вид(Lamb, Veal, Pork or Chicken) через пробіл");
                             input += Console.ReadLine();
                             input += "\n";
                             break;
                         case 2:
-                            Console.WriteLine($"Введіть назву, ціну, вагу та термін придатності через пробіл");
+                            Console.WriteLine($"Введіть назву, ціну, вагу, термін придатності та дату виготовлення через пробіл");
                             input += Console.ReadLine();
                             input += "\n";
                             break;
                         case 3:
-                            Console.WriteLine($"Введіть назву, ціну та вагу через пробіл");
+                            Console.WriteLine($"Введіть назву, ціну, вагу, термін придатності та дату виготовлення через пробіл");
                             input += Console.ReadLine();
                             input += "\n";
                             break;
@@ -58,7 +59,7 @@ namespace Task1
             }
             else if (regime == 2)
             {
-                input += "3\n3\nApple 5.50 0.120\n3\nSugar 30 1\n3\nBread 15 0.5";
+                input += "3\n3\nApple 5.50 0.120 20 15.9.2021\n3\nSugar 30 1 90 23.12.2020\n3\nBread 15 0.5 3 05.10.2021";
             }
 
             return input;
